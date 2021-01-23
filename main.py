d88af7757b4c7829ca1520b827a619cd314d4f59
repl.it user <replituser@example.com>
2021-01-23 10:51:14 +0000

    get each link and send request

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from bs4 import BeautifulSoup
 import requests
 from selenium import webdriver
 from fake_useragent import UserAgent
+from time import sleep
+from random import randint
 
 ua = UserAgent()
 
@@ -9,11 +11,8 @@ headers = {
   'User-Agent': ua.ie
 }
 
-# browser = webdriver.Firefox()
-# browser.get('https://www.leathercountrybags.com/category.cfm')
-# button = browser.find_element_by_css_selector('a.btn.btn-sm.btn-info')
-
-# print(browser)
+browser = webdriver.Firefox()
+browser.get('https://www.leathercountrybags.com/category.cfm')
 
 # Send request to the website
 response = requests.get('https://www.leathercountrybags.com/category.cfm?categoriaid_rw=wholesale-handbags&ord=5', headers=headers)
@@ -25,8 +24,30 @@ soup = BeautifulSoup(response.text, 'html.parser')
 
 products = soup.find_all(class_='btn btn-sm btn-info')
 
+i = 0
+
+items = []
+
 # Loop through each product
 for product in products:
   # Get the links for detailed info
   item = product['onclick'].split(',')[2].replace("'", "").replace(";", "")
-  print(item)
+  items.append(item)
+
+# print(items)
+
+for item in items:
+
+  if i <= 3:
+    detail = requests.get('https://www.leathercountrybags.com/' + item, headers=headers)
+
+    soup_dt = BeautifulSoup(detail.text, 'html.parser')
+
+    p = soup_dt.find_all('p')
+
+    print(p)
+    
+    sleep_time = randint(2, 15) # random numbers for waiting time
+    sleep(sleep_time) # pause the loop for some time
+
+    i += 1
