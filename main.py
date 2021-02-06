0653173f4d8bf9e8fdcc23ca35f597b449a724db
akmatoff <azim.akmatov1@gmail.com>
2021-02-06 17:05:47 +0600

    Scraping method changed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from bs4 import BeautifulSoup
-from selenium import webdriver
 import requests
 from fake_useragent import UserAgent
 from time import sleep
@@ -16,77 +15,48 @@ headers = {
 }
 
 def scrape_data():
-  driver = webdriver.Firefox(executable_path='geckodriver.exe')
 
   # Send request to the website
-  driver.get(url + 'category.cfm?categoriaid_rw=wholesale-handbags&ord=5')
+  response = requests.get(url + 'category.cfm?categoriaid_rw=wholesale-handbags&ord=5', headers)
 
   # Get the page as text
-  content = driver.page_source
+  content = response.text
 
   # Initialize beautiful soup and parse the response text
   soup = BeautifulSoup(content, 'lxml')
-  driver.quit()
 
-  products = soup.find_all(class_='btn btn-sm btn-info')
+  products = soup.select('div.col-sm-7')
+  product_num = 1
 
-  items = []
-
-  # Loop through each product
   for product in products:
-    # Get the links for detailed info
-    item = product['onclick'].split(',')[2].replace("'", "").replace(";", "").replace(')', '')
-    items.append(item)
+    print('Scraping product number ' + str(product_num))
 
-  for item in items: 
-
-    print('Looping through each item...')
-    detail = requests.get(url + item, headers=headers)
-
-    soup_dt = BeautifulSoup(detail.text, 'lxml')
-
-    title = translator.translate(soup_dt.find('h3').find('span').get_text(), dest='ru').text
-    descs = soup_dt.find(class_='col-md-7').find_all('p')
-
-    description = []
+    title = translator.translate(product.find('span', attrs={'class': None}).text, dest='ru').text
+    images = product.select('img.lazy')
+    description = translator.translate(product.find_all('p')[2].text, dest='ru').text
     pics = []
-    images = soup_dt.select('img.item-thumbnail')
-    ul = soup_dt.find('ul').select('li')
 
-    i = 2
-
-    # Loop through specific range of elements in description
-    while i < len(descs) - 4:
-      description.append(translator.translate(descs[i].text, dest='ru').text)
-      i += 1
-
-    print('Description:', description)
-
-    # Get additional description
-    for el in ul:
-      text = translator.translate(el.text, dest='ru').text
-      description.append(text)
-
-    # Loop through each image and save the files
+    # Loop through each image of a product
     for image in images:
-      image_link = url + image.get('src')
-      img_request = requests.get(image_link, 'lxml')  
-      imgname = image.get('src').split('/')[3]
-      pics.append('/images/' + imgname)
+      print('Saving images...')
 
+      img = image['data-original']
+      img_link = url + img
+      img_request = requests.get(img_link, headers)  
+      imgname = img.split('/')[3]
+      pics.append(imgname)
+
+      # Save images
       with open('images/' + imgname, 'wb') as f:
         f.write(img_request.content)
 
-      print('Saving image...')
-
-      sleep(randint(2, 7))
-
-    sleep(randint(3, 10))
-
+  
     with open('products.csv', 'a', encoding='utf-8') as csv_file:
-      csv_writer = writer(csv_file)
-      # Write the received information to a csv file
-      csv_writer.writerow([title, '|'.join(description), '|'.join(pics)])
-      print('Writing csv file...')
+        csv_writer = writer(csv_file)
+        # Write the received information to a csv file
+        csv_writer.writerow([title, description, '|'.join(pics)])
+        print('Writing csv file...')
+    
+    product_num += 1
 
 scrape_data()
